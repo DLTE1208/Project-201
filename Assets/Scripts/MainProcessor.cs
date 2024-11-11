40e38a230746b1669169dd7f58249116d212936d
Shi Yilin <1774873177@qq.com>
2024-11-11 22:45:23 +0800

    1

--- Assets/Scripts/MainProcessor.cs
+++ Assets/Scripts/MainProcessor.cs
@@ -6,6 +6,7 @@ public class MainProcessor : MonoBehaviour
 {
     private string connectionString = "server = DESKTOP-D65RF5L\\MSSQLSERVER2; uid = admin; pwd = 123456; database = RestaurantOfDLT";  //用于链接的数据库的字符串
     //private CustomerFunctions myFunctions;
+    //second
 
     private void Awake()
     {
